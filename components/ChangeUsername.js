c231b70946d8fbcdf4681a7a9e7a949a42600ed5
Savvas Mantzouranidis <mohito6@hotmail.com>
2021-12-18 16:16:02 +0200

    done reactiveness of header for mobile

--- components/ChangeUsername.js
+++ components/ChangeUsername.js
@@ -17,7 +17,7 @@ function ChangeUsername() {
     <button
       disabled={isUserUpdating}
       onClick={setUsername}
-      className="bg-yellow-500 shadow-md shadow-yellow-500/30 text-black rounded-full px-3 py-1 transform hover:scale-105 transition ease-out duration-200"
+      className="bg-yellow-500 shadow-md shadow-yellow-500/30 text-black rounded-full px-2 xs:px-3 py-1 transform hover:scale-105 transition ease-out duration-200"
     >
       Change Username
     </button>
--- components/Header.js
+++ components/Header.js
@@ -6,26 +6,26 @@ function Header() {
   const { user, logout } = useMoralis();
 
   return (
-    <div className="sticky z-50 md:flex text-blue-400 text-center md:text-left md:justify-center md:align-center md:space-x-5 p-4 bg-black/75 shadow-md border-b-2 border-blue-400">
-      <div className="relative md:left-0 md:translate-x-0 left-[50%] translate-x-[-50%] h-48 w-48 border-4 border-blue-400 rounded-full">
+    <div className="sticky z-50 flex text-blue-400 justify-center align-center space-x-2 xs:space-x-4 p-3 sm:p-4 bg-black/75 shadow-sm border-b-2 border-blue-400">
+      <div className="relative h-28 w-28 xs:h-32 xs:w-32 sm:h-48 sm:w-48 border-4 border-blue-400 rounded-full">
         <Avatar />
       </div>
-      <div className="my-auto space-y-4 md:space-y-5">
-        <h1 className="text-2xl text-yellow-500 pt-4 md:pt-0">
+      <div className="my-auto space-y-2 sm:space-y-5">
+        <h1 className="xs:text-xl sm:text-2xl text-yellow-500 sm:pt-0">
           Welcome to the METAVERSE!
         </h1>
-        <div>
-          <h2 className="text-lg">You username is</h2>
-          <h2 className="text-3xl text-blue-300 font-bold truncate">
+        <div className="-space-y-1">
+          <h2 className="text-sm xs:text-lg">You username is</h2>
+          <h2 className="text-lg xs:text-2xl sm:text-3xl text-blue-300 font-bold truncate">
             {user.getUsername()}
           </h2>
         </div>
         <div>
-          <div className="space-x-3 text-sm">
+          <div className="space-x-2 xs:space-x-3 text-sm">
             <ChangeUsername />
             <button
               onClick={logout}
-              className="bg-red-500 shadow-md shadow-red-500/30 text-white rounded-full px-3 py-1 transform hover:scale-105 transition ease-out duration-200"
+              className="bg-red-500 shadow-sm shadow-red-500/30 text-white rounded-full px-2 xs:px-3 py-1 transform hover:scale-105 transition ease-out duration-200"
             >
               Logout
             </button>
